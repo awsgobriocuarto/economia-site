6663d77cee7f79acfd2b8dd1194b27cbfe44cf72
germansayago <german.sayago@gmail.com>
2022-03-02 14:00:15 -0300

    added sass and bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Sec. de Economia Río Cuarto</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Sec. de Economía</h1>
+      <main>
+        <h1>Sec. de Economía</h1>
 
-        <p className={styles.description}>Gobierno de Río Cuarto</p>
+        <p>Gobierno de Río Cuarto</p>
       </main>
     </div>
   );
