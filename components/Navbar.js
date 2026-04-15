9836fd83e3fa46257c8a8f743c894cc3e78a2259
Leandro <lfunes@riocuarto.gov.ar>
2026-04-15 10:47:09 -0300

    feat: add responsive Navbar component with navigation links

--- components/Navbar.js
+++ components/Navbar.js
@@ -37,62 +37,18 @@ export default function Navbar() {
                 </Link>
               </li>
               <li className="nav-item">
-                <Link href="/pagos-y-deudas">
-                  <a className="nav-link">Pagos y Deudas</a>
-                </Link>
-              </li>
-              <li className="nav-item d-none">
-                <Link href="/compras-web">
-                  <a className="nav-link">Compras Web</a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/tramites-y-servicios">
-                  <a className="nav-link">Trámites y Servicios</a>
-                </Link>
-              </li>
-              <li className="nav-item">
                 <Link href="/transparencia">
                   <a className="nav-link">Transparencia</a>
                 </Link>
               </li>
               <li className="nav-item">
+                <Link href="/innovacion">
+                  <a className="nav-link">Innovación</a>
+                </Link>
+              </li>
+              <li className="nav-item">
                 <Link href="/legislacion">
                   <a className="nav-link">Legislación</a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/sustentabilidad">
-                  <a className="nav-link">Sustentabilidad</a>
-                </Link>
-              </li>
-              {/* <li className="nav-item">
-              <Link href="/ods">
-                <a className="nav-link">ODS</a>
-              </Link>
-            </li> */}
-              <li className="nav-item">
-                <a
-                  href="https://admin.toteminsight.com/progressiveApp/5f04b1401320d01ab4a513f6/index.jade"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="nav-link"
-                >
-                  Turnos Web
-                </a>
-              </li>
-              <li className="nav-item">
-                <Link
-                  href={
-                    process.env.NODE_ENV == "development"
-                      ? "http://localhost:3001"
-                      : "https://comprasweb.economiariocuarto.gob.ar/"
-                  }
-                  passHref
-                >
-                  <a className="nav-link special">
-                    Compras Web <i className="fas fa-sign-out-alt"></i>
-                  </a>
                 </Link>
               </li>
             </ul>
