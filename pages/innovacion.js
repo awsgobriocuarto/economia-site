bcbfd799cac5b8dfd046a0570912ed1f9523271c
Leandro <lfunes@riocuarto.gov.ar>
2026-05-19 14:32:37 -0300

    feat: add new Innovacion page with placeholder content

--- pages/innovacion.js
+++ pages/innovacion.js
@@ -17,12 +17,11 @@ export default function Innovacion() {
           <SectionHeader title="DESARROLLOS E INNOVACIÓN" />
 
           <div className="row g-4 news mt-3">
-
-            {
+            {/* 
               innovacionPosts.map((post) => (
                 <PostCard key={post.id} post={post} />
               ))
-            }
+            */}
 
             <div className="col-12 text-center py-5 my-5">
               <h4 className="text-muted fw-normal">Próximamente</h4>
